448171b4d4d3094f31e8b176d100ee4d5b801bfa
frafra <fraph24@gmail.com>
2008-07-26 00:42:44 +0200

    rename everything with regex

---
+++ reverything.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- utf-8 -*-
+#
+# reverything.py
+#
+# Copyright 2008 Francesco Frassinelli <fraph24@gmail.com>
+# 
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#    
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#    
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from os import getcwd, listdir, rename
+from re import search
+from sys import argv
+
+print 'Usage: %s <filter> <name>' % argv[0]
+
+regex = argv[1]
+name = argv[2]
+
+items = {}
+
+for item in listdir(getcwd()):
+    res = search(regex, item)
+    if res:
+        # Syntax like: '%(0)s' % {'0':res.group(0)}
+        groups = res.groups()
+        replace = dict(zip([str(n) for n in xrange(len(groups))], groups))
+        items[item] = name % replace
+
+print items
+if raw_input('Ok? [y/n] ') != 'y': raise SystemExit
+
+for item in items:
+    rename(item, items[item])
+
