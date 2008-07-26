aac8742fa1983a758a3da4e9e35177f88ac9c71f
frafra <fraph24@gmail.com>
2008-07-27 00:31:59 +0200

    rewritten; now support multiple working directories

--- reverything.py
+++ reverything.py
@@ -20,47 +20,55 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
-from re import search
-from sys import argv
+import os, re, sys
 
-# Syntax like: '%(0)s' % {'0':res.group(0)}
+# Syntax example: 'dsc(\d+)' -> 'photo-%(1)s' % {'1':res.group(0)}
+# See also: http://docs.python.org/lib/re-syntax.html
 
 class Reverything:
-    def __init__(self, regex, name, directory=os.getcwd()):
-        self.regex = regex
-        self.name = name
-        self.directory = directory
-    def ls(self):
+    ''' Rename multiple files and/or directories using 2 regex '''
+    def __init__(self, regex_in, regex_out, dirs):
+        self.regex_in, self.regex_out = regex_in, regex_out
+        self.dirs = dirs
+    def preview(self):
+        ''' See what files match with the given regex and memorize new names '''
         self.items = {}
-        for item in os.listdir(self.directory):
-            re = search(self.regex, item)
-            if not re: continue
-            res = re.groups()
-            replace = dict(zip([str(n) for n in xrange(len(res))], res))
-            self.items[item] = self.name % replace
-    def rn(self):
-        for item in self.items:
-            os.rename(os.path.join(self.directory, item),
-                   os.path.join(self.directory, self.items[item]))
+        for d in self.dirs:
+            # Calls re.search(self.regex_in, i), we should optimize it
+            old = [i for i in os.listdir(d) if re.search(self.regex_in, i)]
+            new = []
+            for item in old:
+                res = re.search(self.regex_in, item).groups()
+                replace = dict(zip([str(n+1) for n in xrange(len(res))], res))
+                new.append(self.regex_out % replace)
+            self.items[d] = dict(zip(old, new))
+        # We need to implement a cleanup action for empty results
+    def apply(self):
+        ''' Rename everything '''
+        for d in self.items:
+            for item in self.items[d]:
+                os.rename(os.path.join(d, item),
+                          os.path.join(d, self.items[d][item]))
 
 def main():
-    if len(argv) not in (3, 4):
-        print 'Usage: %s <filter> <name> [directory]' % argv[0]
-        raise SystemExit
-    
-    rename = Reverything(*argv[1:])
-    rename.ls()
-    if len(rename.items):
-        print 'Preview (into %s):' % rename.directory
-        for item in rename.items:
-            print '  %s -> %s' % (item, rename.items[item])
+    ''' Example code for use Reverything class '''
+    if len(sys.argv) < 3:
+        print 'Usage: %s <filter> <name> [directories]' % sys.argv[0]
+        sys.exit(0)
+    if len(sys.argv) < 4:
+        dirs = [os.getcwd()]
     else:
-        print 'Nothing to rename.'
-        raise SystemExit
+        dirs = sys.argv[3:]
+    rename = Reverything(sys.argv[1], sys.argv[2], dirs)
+    rename.preview()
+    for d in rename.items:
+        print 'Preview (into %s):' % d
+        for item in rename.items[d]:
+            print '    %s -> %s' % (item, rename.items[d][item])
     
-    if raw_input('Rename? [y/n] ').lower() == 'y': rename.rn()
+    if raw_input('Rename? [y/n] ').lower() == 'y':
+        rename.apply()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
 
